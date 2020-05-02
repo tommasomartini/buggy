9ef3f64a08b21b706a7802b565291b473f77d1be
TomPi <2669009+tommasomartini@users.noreply.github.com>
2020-05-02 16:07:38 +0200

    Motion in 4 directions with keyboard

--- robot/motion.py
+++ robot/motion.py
@@ -1,16 +1,30 @@
+from gpiozero import Robot
+import time
 from pynput import keyboard
 
-is_going = False
+print('Start')
 
-_GO_KEY = keyboard.Key.up
+robby = Robot(left=(7, 8), right=(9, 10))
+is_going = False
+_FW_KEY = keyboard.Key.up
+_BW_KEY = keyboard.Key.down
+_L_KEY = keyboard.Key.left
+_R_KEY = keyboard.Key.right
 
 
 def on_press(key):
     global is_going
-    if key == _GO_KEY:
+    if key == _FW_KEY:
         if not is_going:
             is_going = True
-            print('Start')
+            #print('Start')
+            robby.forward()
+    elif key == _BW_KEY:
+        robby.backward()
+    elif key == _L_KEY:
+        robby.left()
+    elif key == _R_KEY:
+        robby.right()
 
 
 def on_release(key):
@@ -18,11 +32,13 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener.
         is_going = False
+        robby.stop()
         return False
 
-    if key == _GO_KEY:
+    if key in (_FW_KEY, _BW_KEY, _L_KEY, _R_KEY):
         is_going = False
-        print('Stop')
+        robby.stop()
+        # print('Stop')
 
 # Collect events until released
 
@@ -31,8 +47,5 @@ with keyboard.Listener(
         on_release=on_release) as listener:
     listener.join()
 
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
+robby.stop()
+
