eac067724f59959de3ac2e93e17548c9fa6384c7
TomPi <2669009+tommasomartini@users.noreply.github.com>
2020-05-23 17:55:19 +0200

    Read 5 keys and suppress output to console

--- robot/controller.py
+++ robot/controller.py
@@ -3,25 +3,32 @@ import time
 from gpiozero import Robot
 from pynput import keyboard
 
-_KEYS_F = tuple([keyboard.KeyCode.from_char(c) for c in ('w', 'W')])
-_KEYS_B = tuple([keyboard.KeyCode.from_char(c) for c in ('w', 'W')])
-_KEYS_L = tuple([keyboard.KeyCode.from_char(c) for c in ('a', 'A')])
-_KEYS_R = tuple([keyboard.KeyCode.from_char(c) for c in ('d', 'D')])
-_KEY_TURBO = keyboard.Key.shift_l
+_key_to_index = {
+    # Forward
+    keyboard.KeyCode.from_char('w'): 0,
+    keyboard.KeyCode.from_char('W'): 0,
+    
+    # Backward
+    keyboard.KeyCode.from_char('s'): 1,
+    keyboard.KeyCode.from_char('S'): 1,
 
-_key_to_index = {k: idx for idx, k in enumerate([
-    _KEYS_F,
-    _KEYS_B,
-    _KEYS_L,
-    _KEYS_R,
-    _KEY_TURBO,
-])}
+    # Left
+    keyboard.KeyCode.from_char('a'): 2,
+    keyboard.KeyCode.from_char('A'): 2,
+    
+    # Right
+    keyboard.KeyCode.from_char('d'): 3,
+    keyboard.KeyCode.from_char('D'): 3,
+    
+    # Turbo
+    keyboard.Key.shift_l: 4,
+}
+_commands = [0] * len(set(_key_to_index.values()))
 
-# [f, b, l, r, turbo]
-_keys = [0] * 5
 _mv_time = 1.
 
 robot = Robot(left=(18, 12), right=(19, 13))
+
 
 def move():
     return
@@ -37,26 +44,20 @@ def move():
     speed = 0.5 + _keys[1] * 0.5
     robot.forward(speed)
 
+
 def _on_press(key):
-    #index = _key_to_index.get(key)
-    #if index is None:
+    index = _key_to_index.get(key)
+    if index is None:
+        return
 
+    if _commands[index]:
+        # Command already set: no need to do anything.
+        return
 
-    if key in _KEYS_F:
-        if _keys[0]:
-            # Already set: do not do anything.
-            return
+    # Set this command.
+    _commands[index] = 1
 
-        print('+f')
-        _keys[0] = 1
-
-    elif key == _KEY_TURBO:
-        if _keys[1]:
-            # Already set: do not do anything.
-            return
-
-        print('+t')
-        _keys[1] = 1
+    print('+{}'.format(index))
 
     move()
 
@@ -65,17 +66,21 @@ def _on_release(key):
     if key == keyboard.Key.esc:
         return False
 
-    if key in _KEYS_F and _keys[0]:
-        print('-f')
-        _keys[0] = 0
+    index = _key_to_index.get(key)
+    if index is None:
+        return
 
-    elif key == _KEY_TURBO and _keys[1]:
-        print('-t')
-        _keys[1] = 0
+    # Unset this command.
+    _commands[index] = 0
+
+    print('-{}'.format(index))
 
     move()
 
-with keyboard.Listener(on_press=_on_press, on_release=_on_release) as listener:
+
+with keyboard.Listener(on_press=_on_press,
+                       on_release=_on_release,
+                       suppress=True) as listener:
     listener.join()
 
 robot.close()
