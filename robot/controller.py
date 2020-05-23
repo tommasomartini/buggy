8a75049107ad3757948212d8ad39c9bfbe307156
TomPi <2669009+tommasomartini@users.noreply.github.com>
2020-05-23 21:59:11 +0200

    Distance sensor interrupts motion

--- robot/controller.py
+++ robot/controller.py
@@ -1,7 +1,13 @@
+import logging
+import threading
 import time
 
-from gpiozero import Robot
+from gpiozero import DistanceSensor, LED, Robot
 from pynput import keyboard
+
+logging.basicConfig(level=logging.DEBUG, style='{', format='[{threadName}][{name}][{levelname}] {message}')
+_logger = logging.getLogger(__name__)
+
 
 _key_to_index = {
     # Forward
@@ -25,73 +31,124 @@ _key_to_index = {
 }
 _commands = [0] * len(set(_key_to_index.values()))
 
+_engage_safety_stop_event = threading.Event()
+_dismiss_safety_stop_event = threading.Event()
 _mv_time = 1.
 
+# Devices
 robot = Robot(left=(18, 12), right=(19, 13))
+led = LED(17)
+
+
+def _when_in_range():
+    _logger.debug('Obstacle in range')
+    _engage_safety_stop_event.set()
+    led.blink(on_time=0.1, off_time=0.1)
+
+
+def _when_out_of_range():
+    _logger.debug('Clear way')
+    _engage_safety_stop_event.clear()
+    _dismiss_safety_stop_event.set()
+    led.off()
+
+
+distance_sensor = DistanceSensor(echo=7, trigger=25, threshold_distance=5e-2)
+distance_sensor.when_in_range = _when_in_range
+distance_sensor.when_out_of_range = _when_out_of_range
+
+
+def _safety_stop():
+    while True:
+        _engage_safety_stop_event.wait()
+        _logger.debug('Emergency stop!')
+
+        # Stop the robot and reset all the commands.
+        robot.stop()
+        for idx in range(4):
+            _commands[idx] = 0
+
+        _dismiss_safety_stop_event.wait()
+        _logger.debug('Dismiss emergency stop')
+        _dismiss_safety_stop_event.clear()
+
+
+safety_stop_thread = threading.Thread(target=_safety_stop, name='safety_stop', daemon=True)
+safety_stop_thread.start()
 
 
 def move():
-    # Evaluate the commands provided and set the motors.
+    _logger.debug('Move. Commands: {}'.format(_commands))
+
     if sum(_commands[:4]) == 0:
-        # Nothing to do.
+        # No move action to perform.
+        _logger.debug(' Stop')
         robot.stop()
         return
 
     # Setting both "forward" and "backward" or "left" and "right" not allowed.
     # Maintain the current course.
     if (_commands[0] and _commands[1]) or (_commands[2] and _commands[3]):
+        _logger.debug(' Invalid command configuration')
         return
 
     speed = 1.0 if _commands[4] else 0.5
 
-    if not _commands[0] and not _commands[1]:
+    if (not _commands[0]) and (not _commands[1]):
         # Only left-right provided.
+        _logger.debug(' Only left-right')
         mv_func = robot.left if _commands[2] else robot.right
+        # assert _commands[3]
         mv_func(speed)
     else:
+        _logger.debug(' Forward-backward')
         mv_func = robot.forward if _commands[0] else robot.backward
 
         # Any lateral movement?
         kwargs = dict(speed=speed)
         if _commands[2]:
+            _logger.debug('  + left')
             kwargs['curve_left'] = 0.5
-        elif _commands[3]:
+        if _commands[3]:
+            _logger.debug('  + right')
             kwargs['curve_right'] = 0.5
 
         mv_func(**kwargs)
+
+    _logger.debug('--- end move() ---')
+
+
+def _set_command(key, value):
+    if _engage_safety_stop_event.is_set():
+        # Ignore every command until it is safe to proceed.
+        return
+
+    index = _key_to_index.get(key)
+    if index is None:
+        # Unrecognized command.
+        return
+
+    if _commands[index] == value:
+        # Command already set: no need to do anything.
+        return
+
+    # Set this command.
+    _commands[index] = value
+
+    move()
 
 
 def _on_press(key):
     index = _key_to_index.get(key)
     if index is None:
         return
-
-    if _commands[index]:
-        # Command already set: no need to do anything.
-        return
-
-    # Set this command.
-    _commands[index] = 1
-
-    print('+{}'.format(index))
-
-    move()
+    _set_command(key, 1)
 
 
 def _on_release(key):
     if key == keyboard.Key.esc:
         return False
-
-    index = _key_to_index.get(key)
-    if index is None:
-        return
-
-    # Unset this command.
-    _commands[index] = 0
-
-    print('-{}'.format(index))
-
-    move()
+    _set_command(key, 0)
 
 
 with keyboard.Listener(on_press=_on_press,
@@ -99,6 +156,9 @@ with keyboard.Listener(on_press=_on_press,
                        suppress=True) as listener:
     listener.join()
 
+led.off()
+led.close()
+distance_sensor.close()
 robot.close()
 print('Bye')
 
