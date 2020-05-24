869d9d066d69ecb0fac453b3973b7389b844e9ed
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-05-24 15:21:27 +0200

    Add UltrasonicSensor class with callbacks

--- robot/sensor/ultrasonic.py
+++ robot/sensor/ultrasonic.py
@@ -20,7 +20,7 @@ class _TimeoutError(Exception):
     pass
 
 
-class BaseUltrasonicSensor:
+class UltrasonicSensor:
     """Implements the basic functionality to use an ultrasonic distance sensor
     like the common HC-SR04.
 
@@ -30,6 +30,10 @@ class BaseUltrasonicSensor:
     whose duration is equal to the time between sending the sound signal and
     receiving it back.
 
+    This class provides two callback functions to call when an obstacle gets in
+    range or out of range. The functions are blocking: while the functions do
+    not return, no further measurement will be taken.
+
     Attributes:
         _trig_pin (int): TRIG pin.
         _echo_pin (int): ECHO pin.
@@ -37,6 +41,14 @@ class BaseUltrasonicSensor:
             in seconds.
         _measure_interval_s (float): Minimum time, in seconds, between two
             consecutive measurements to guarantee clean readings.
+        _distance_threshold_m (float, optional): Distance in meters. When the
+            read distance becomes lower than this threshold, the callback
+            "when_in_range" is called. When the read distance becomes bigger,
+            the "when_out_of_rage" is called.
+        _when_in_range (callable, optional): Function to call when the read
+            distance becomes smaller than "_distance_threshold_m".
+        _when_out_of_range (callable, optional): Function to call when the read
+            distance becomes larger than "_distance_threshold_m".
         _name (str, optional): Name of the device.
     """
 
@@ -67,16 +79,25 @@ class BaseUltrasonicSensor:
                  echo_pin,
                  pulse_s,
                  measure_interval_s,
+                 distance_threshold_m=None,
+                 when_in_range=None,
+                 when_out_of_range=None,
                  name=None):
         self._trig_pin = trig_pin
         self._echo_pin = echo_pin
         self._pulse_s = pulse_s
         self._measure_interval_s = \
             measure_interval_s * self._MEASURE_INTERVAL_FACTOR
+        self._distance_threshold_m = distance_threshold_m
+        self._when_in_range = when_in_range
+        self._when_out_of_range = when_out_of_range
         self._name = name
         if self._name is None:
             self._name = 'UltrasonicSensor{}'.format(self._id)
             self._update_id()
+
+        # Keeps track if the measured distance is in range or not.
+        self._in_range = None
 
         _logger.info('Initializing ultrasonic sensor {}'.format(self._name))
 
@@ -98,6 +119,36 @@ class BaseUltrasonicSensor:
         time.sleep(self._pulse_s)
         GPIO.output(self._trig_pin, False)
 
+    def _callbacks(self, distance_cm):
+        """Calls the appropriate callback function, if a status change is
+        detected.
+
+        Args:
+            distance_cm (float): The latest read distance, in cm.
+        """
+        if self._distance_threshold_m is None:
+            return
+
+        if self._when_in_range is None and self._when_out_of_range is None:
+            return
+
+        in_range = distance_cm <= 100 * self._distance_threshold_m
+        if in_range == self._in_range:
+            # No status change.
+            return
+
+        # Detected a status change.
+        self._in_range = in_range
+
+        if self._in_range:
+            # Out of -> in range.
+            if self._when_in_range is not None:
+                self._when_in_range()
+        else:
+            # In -> out of range.
+            if self._when_out_of_range is not None:
+                self._when_out_of_range()
+
     def read(self):
         """Cycles forever yielding distance measurements in cm.
         """
@@ -113,7 +164,6 @@ class BaseUltrasonicSensor:
                     raise _TimeoutError()
 
                 pulse_start = time.time()
-
                 channel = GPIO.wait_for_edge(self._echo_pin,
                                              GPIO.FALLING,
                                              timeout=timeout_ms)
@@ -121,10 +171,10 @@ class BaseUltrasonicSensor:
                     raise _TimeoutError()
 
                 pulse_end = time.time()
-
                 distance_cm = (pulse_end - pulse_start) \
                               * self._PULSE_TO_DISTANCE_MULTIPLIER_cmps
                 yield distance_cm
+                self._callbacks(distance_cm=distance_cm)
 
             except _TimeoutError:
                 _logger.warning(
@@ -139,10 +189,15 @@ class BaseUltrasonicSensor:
 
 def _tryout():
     GPIO.setmode(GPIO.BCM)
-    sensor = BaseUltrasonicSensor(trig_pin=25,
-                                  echo_pin=8,
-                                  pulse_s=10e-6,
-                                  measure_interval_s=60e-3)
+    sensor = UltrasonicSensor(
+        trig_pin=25,
+        echo_pin=8,
+        pulse_s=10e-6,                                  # 10 us
+        measure_interval_s=60e-3,                       # 60 ms
+        distance_threshold_m=0.1,                       # 10 cm
+        when_in_range=lambda: print('In range'),
+        when_out_of_range=lambda: print('Out of range'),
+        name='MySonar')
 
     try:
         for distance_cm in sensor.read():
