abb6e55e59f4a871ccf5c6bd5457d174f011f7dd
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-05-24 11:47:19 +0200

    Clean up main robot module

--- robot/robot.py
+++ robot/robot.py
@@ -1,54 +1,16 @@
 import logging
 
-from gpiozero import Robot
+import robot.motion.keyboard_driver as keyboard_driver
 
-import common.network as network
-
-_STRING_ENCODING = 'utf-8'
+logging.basicConfig(level=logging.DEBUG,
+                    style='{',
+                    format='[{threadName}][{name}][{levelname}] {message}')
 
 
 def _main():
-    _logger.info('Start robot')
-
-    server = network.UDPServer(ip=network.RASPBERRYPI_HOSTNAME,
-                               port=network.PORT)
-
-    robot = Robot(left=(7, 8), right=(9, 10))
-    is_moving = False
-
-    try:
-        for data in server.receive():
-            data_str = data.decode(_STRING_ENCODING)
-            if data_str == 'f' and not is_moving:
-                is_moving = True
-                robot.forward()
-
-            elif data_str == 'b' and not is_moving:
-                is_moving = True
-                robot.backward()
-
-            elif data_str == 'l' and not is_moving:
-                is_moving = True
-                robot.left()
-
-            elif data_str == 'r' and not is_moving:
-                is_moving = True
-                robot.right()
-
-            elif data_str == 's':
-                is_moving = False
-                robot.stop()
-
-    except KeyboardInterrupt:
-        pass
-
-    finally:
-        robot.stop()
-        server.close()
-        _logger.info('Stop robot')
+    keyboard_driver.KeyboardDriver()
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    _logger = logging.getLogger(__name__)
     _main()
+
