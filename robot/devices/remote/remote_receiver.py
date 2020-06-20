bba8335b8a587a921ea7382d69fc96f598fec8a2
tommasomartini <2669009+tommasomartini@users.noreply.github.com>
2020-06-20 23:02:36 +0200

    Add timeout to remote receiver

--- robot/devices/remote/remote_receiver.py
+++ robot/devices/remote/remote_receiver.py
@@ -1,4 +1,5 @@
 import logging
+import signal
 
 import common.network as network
 import robot.devices.remote.common as common
@@ -12,10 +13,25 @@ class RemoteReceiver:
     user's side.
     """
 
+    # The remote sends a signal to start the motors and a signal to stop
+    # them. If communication breaks in between, risk is that motors will
+    # never stop. For this reason, the sender has to keep sending data to
+    # the receiver. If data is not received for a large enough interval,
+    # the receiver will interpret it as a communication breakdown and will
+    # stop the motors automatically.
+    _NO_SIGNAL_RECEIVED_TIMEOUT_s = 1.0
+
     def __init__(self, driver):
         self._driver = driver
         self._server = network.UDPServer(ip=network.RASPBERRYPI_HOSTNAME,
                                          port=network.PORT)
+
+        signal.signal(signal.SIGALRM, self._on_timeout)
+
+    def _on_timeout(self):
+        _logger.warning('Signal from remote stopped unexpectedly: '
+                        'stop the motors')
+        self._driver.stop()
 
     def run(self):
         _logger.debug('{} started'.format(self.__class__.__name__))
@@ -53,7 +69,15 @@ class RemoteReceiver:
                     self._driver.set_command(command_code=dvr.COMMAND_TURBO,
                                              command_value=False)
 
+                # Reset the timer to trigger an alarm signal if the next signal
+                # from the sender takes too long.
+                signal.setitimer(signal.ITIMER_REAL,
+                                 self._NO_SIGNAL_RECEIVED_TIMEOUT_s,
+                                 0)
+
         finally:
+            # Disable the alarm.
+            signal.setitimer(signal.ITIMER_REAL, 0, 0)
             self._server.close()
 
         _logger.debug('{} stopped'.format(self.__class__.__name__))
